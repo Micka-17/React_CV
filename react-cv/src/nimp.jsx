6f9850bbe70db266e3b591ac6252e96a922e2975
Micka17 <mickael.boi@sfr.fr>
2021-10-25 10:44:11 +0200

    first commit

---
+++ react-cv/src/nimp.jsx
@@ -0,0 +1,17 @@
+import Top from './components/Top'
+import Download from './components/Download'
+import Footer from './components/Footer'
+import Skills from './components/Skills'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Top />,
+      <Skills />,
+      <Download />,
+      <Nav />,
+      <Footer />
+    </div>
+  );
+}
